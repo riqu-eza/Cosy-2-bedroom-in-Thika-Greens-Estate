c0b9c679a1f3e37b0e6a3f2512a44de79d30b206
offline <vickymuthunga@gmail.com>
2024-11-29 20:27:07 +0300

    well

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3004',
+        target: 'http://cosytwobedroominthika.online/',
         secure: false,
       },
     },
